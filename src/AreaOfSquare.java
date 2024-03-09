a0efd8f969d0263eef65ca63bd958599e54e607e
Bindiya patel <bindiyapatel190585@gmail.comgit config --global user.email bindiyapatel190585@gmail.com>
2024-03-09 14:33:29 +0000

    new change

--- src/AreaOfSquare.java
+++ src/AreaOfSquare.java
@@ -3,6 +3,7 @@ public class AreaOfSquare {
     {
         double a = (b*b);
         System.out.println("Area of Square is for given dimension of side "+b+" is :" + a);
+        System.out.println( "Area of square");
     }
     public static void main(String[]args){
         AreaOfSquare AreaOfSquare=new AreaOfSquare();
